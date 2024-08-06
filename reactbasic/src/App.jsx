ab4ce15d4542f71f6bf97af0df88d794266438cf
samir-k1 <rockoncode12@gmail.com>
2024-08-06 18:18:34 +0530

    first commit

---
+++ reactbasic/src/App.jsx
@@ -0,0 +1,17 @@
+
+import New from './new';
+
+
+function App() {
+  
+
+  return (
+    <>
+    <New/>
+    <h1>bnm,xcvb,mn</h1>
+    
+    </>
+  )
+}
+
+export default App
---
+++ reactbasic/src/new.jsx
@@ -0,0 +1,13 @@
+
+
+function New() {
+    return (
+      <>
+        <h1>dfghjkl</h1>
+      </>
+    );
+  }
+  
+  export default New;
+
+
